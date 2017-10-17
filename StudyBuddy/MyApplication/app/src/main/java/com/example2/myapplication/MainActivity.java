684489bf428612e7fbc8ed3e82d2afb60dd0c3b1
caitlinard <card@nmsu.edu>
2017-10-17 15:34:23 -0600

    Added Angela's changes to login

---
+++ StudyBuddy/MyApplication/app/src/main/java/com/example2/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example2.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ShareCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+    }
+
+    public void goToLogin (View view){
+        Intent intent= new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+
+}
