7937f4de259daef8222d6690579f4718d4bbbda5
Caitlin Ard <card@nmsu.edu>
2017-12-02 18:52:11 -0700

    added user creation communication

--- StudyBuddy/MyApplication/app/src/main/java/com/example2/myapplication/CreateGroup.java
+++ StudyBuddy/MyApplication/app/src/main/java/com/example2/myapplication/CreateGroup.java
@@ -13,28 +13,26 @@ import android.view.View;
 public class CreateGroup extends Activity {
     public void toCreateGroup(View view) {
 
-        private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener;
-        mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.navigation_home:
-                mTextMessage.setText(R.string.title_home);
-                return true;
-            case R.id.navigation_groups:
-                mTextMessage.setText("Groups");
-                return true;
-            case R.id.navigation_profile:
-                mTextMessage.setText("Profile");
-                return true;
-            case R.id.navigation_notifications:
-                mTextMessage.setText(R.string.title_notifications);
-                return true;
-        }
-        return false;
+//        public BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener;
+//        mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+//
+//    @Override
+//    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.navigation_home:
+//                mTextMessage.setText(R.string.title_home);
+//                return true;
+//            case R.id.navigation_groups:
+//                mTextMessage.setText("Groups");
+//                return true;
+//            case R.id.navigation_profile:
+//                mTextMessage.setText("Profile");
+//                return true;
+//            case R.id.navigation_notifications:
+//                mTextMessage.setText(R.string.title_notifications);
+//                return true;
+//        }
+//        return false;
     }
 
 };
-    }
-}
