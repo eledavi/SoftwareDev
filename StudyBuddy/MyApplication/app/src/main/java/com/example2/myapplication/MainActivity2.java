6d784c8009e32fdf230a7e7c8e6bc71eb1e4c578
Caitlin Ard <card@nmsu.edu>
2017-12-03 19:00:17 -0700

    populating home screen

--- StudyBuddy/MyApplication/app/src/main/java/com/example2/myapplication/MainActivity2.java
+++ StudyBuddy/MyApplication/app/src/main/java/com/example2/myapplication/MainActivity2.java
@@ -1,12 +1,16 @@
 package com.example2.myapplication;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+
+// Main activity for profile.
 public class MainActivity2 extends AppCompatActivity {
 
     private TextView mTextMessage;
@@ -43,6 +47,26 @@ public class MainActivity2 extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        setText("firstName", R.id.firstNameText);
+        setText("lastName", R.id.lastNameText);
+        setText("university", R.id.universityText);
+        setText("major", R.id.majorText);
+        setText("email", R.id.emailText);
+
+
+    }
+
+    private void setText(String name, int id) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        TextView view = (TextView) findViewById(id);
+        view.setText(prefs.getString(name, "Wrong!"));
+    }
+
+    public void pr(String msg) {
+        System.out.println(msg);
     }
 
 }
